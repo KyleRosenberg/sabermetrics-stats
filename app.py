fc9ffd92fd5163b1927af390bd67918a27007cf3
KyleRosenberg <kyletherosenberg@icloud.com>
2019-04-21 16:09:18 -0600

    Add equation rendering
    And a basic histogram for the stat

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
-from flask import Flask, jsonify, request, render_template, send_file, send_from_directory, session, Markup, after_this_request, url_for
+from flask import Flask, jsonify, request, render_template, send_file, send_from_directory, session, Markup, after_this_request, url_for, Response
 from io import BytesIO as IO
 import gzip
 import functools
+import json
+import re
+import base64
+import io
 
 from pybaseball.lahman import *
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 import pandas as pd
 
-PITCHING_DATA = pitching()
-BATTING_DATA = batting()
-FIELDING_DATA = fielding()
-TEAM_DATA = teams()
+def rightData(df):
+    dfRet = df.loc[df['yearID']>2006].dropna()
+    return dfRet
+
+PITCHING_DATA = rightData(pitching())
+BATTING_DATA = rightData(batting())
+FIELDING_DATA = rightData(fielding())
+TEAM_DATA = rightData(teams())
 
 GROUPS = ['p', 'b', 'f']
 
@@ -86,6 +97,25 @@ def groupinfo():
         return jsonify(FIELDING_DATA.columns.values.tolist()[6:])
     return 'Something went wrong', 400
 
+@app.route('/visualize', methods=['POST'])
+@gzipped
+def visualize():
+    if 'group' not in request.form:
+        return 'Stat group not specified', 400
+    group = request.form['group']
+    if group not in GROUPS:
+        return 'Invalid stat group', 400
+    if 'equation' not in request.form:
+        return 'Equation not provided', 400
+    equation = request.form['equation']
+    if 'name' not in request.form:
+        return 'Stat name not provided', 400
+    name = request.form['name']
+    dfStats = buildDataframe(json.loads(equation), group, name)
+    print(dfStats[name].max())
+    dist = getDistribution(dfStats, name)
+    return render_template('visualize.html', result=dist.decode('utf8'))
+
 @app.route('/')
 @gzipped
 def default():
@@ -96,6 +126,58 @@ def default():
         top_bar=top_bar,
         stats=PITCHING_DATA.columns.values[7:]
     )
+
+def calculateNewStat(df, equation, name):
+    numerator = []
+    denominator = []
+    for e in equation.keys():
+        nums = equation[e]
+        if nums[1]>0:
+            numerator.append(e)
+        else:
+            denominator.append(e)
+    if len(numerator)==0:
+        df['Numerator'] = 1
+    else:
+        df['Numerator'] = df[numerator].sum(axis=1)
+    if len(denominator)==0:
+        df['Denominator'] = 1
+    else:
+        df['Denominator'] = df[denominator].sum(axis=1)
+    df[name] = df['Numerator']/df['Denominator']
+    return df
+
+def buildDataframe(equation, group, name):
+    df = None
+    if group=='p':
+        df = PITCHING_DATA
+    if group=='b':
+        df = BATTING_DATA
+    if group=='f':
+        df = FIELDING_DATA
+    dfStats = pd.DataFrame(columns=[e for e in equation.keys()])
+    for e in equation.keys():
+        sname = re.sub(r'\d+$', '', e)
+        nums = equation[e]
+        dfStats[e] = nums[0] * (df[sname]**abs(nums[1]))
+    dfRet = calculateNewStat(dfStats, equation, name)
+    return dfRet
+
+def getDistribution(df, name):
+    plt.hist(df[name], density=True)
+    plt.title('Custom Stat: ' + name)
+    plt.ylabel('Frequency')
+    plt.xlabel('Stat Value')
+
+    from io import BytesIO
+    figfile = BytesIO()
+    plt.savefig(figfile, format='png')
+    figfile.seek(0)  # rewind to beginning of file
+    import base64
+    #figdata_png = base64.b64encode(figfile.read())
+    figdata_png = base64.b64encode(figfile.getvalue())
+    plt.clf()
+    return figdata_png
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
--- js/sandbox.js
+++ js/sandbox.js
@@ -1,10 +1,10 @@
-let arr_all_stats = []
-let arr_act_stats = []
+let arr_all_stats = [];
+let dic_act_stats = {};
 
 $(document).ready(function(event) {
     console.log('Sandbox ready!');
     stat_items = $('.stat_item')
-    for (let i = 0; i<stat_items.length; i++){
+    for (let i = 0; i < stat_items.length; i++) {
         si = stat_items[i];
         arr_all_stats.push(si.innerText);
     }
@@ -12,42 +12,72 @@ $(document).ready(function(event) {
     $('.stat_bar .ui.fluid.selection.dropdown').dropdown({
         onChange: getGroupInfo
     });
+    $('#btn_visualize').click(submitStat);
 });
 
-function addStat(){
+function addStat() {
     let st = $(this)[0].name.substring(4);
-    arr_act_stats.push(st);
+    let exp = 1;
+    let key = st + exp;
+    while (key in dic_act_stats) {
+        exp += 1;
+        key = st + exp;
+    }
+    dic_act_stats[key] = [1, 1];
     updateActiveStats()
 }
 
-function removeStat(){
+function removeStat() {
     let st = $(this)[0].name.substring(4);
-    let ind = arr_act_stats.indexOf(st);
-    if (ind != -1){
-        arr_act_stats.splice(ind, 1)
+    if (st in dic_act_stats) {
+        delete dic_act_stats[st]
     }
     updateActiveStats()
 }
 
-function updateActiveStats(){
+function updateActiveStats() {
     let cont = $('.active_bar #stat_container');
     cont.empty();
-    for (let i = 0; i<arr_act_stats.length; i++){
-        stat = arr_act_stats[i]
+    for (stat in dic_act_stats) {
+        info = dic_act_stats[stat];
         cont.append(`<div class="stat_item">
             <button class="ui icon button" name="btn_${stat}">
                 <i class="red minus circle icon"></i>
             </button>
-            <label>${stat}</label>
+            <div class="ui input">
+                <input type="number" name="coef_${stat}" value="${info[0]}">
+            </div>
+            <label> x ${stat.replace(/\d+$/, "")}^</label>
+            <div class="ui input">
+                <input type="number" name="exp_${stat}" value="${info[1]}">
+            </div>
         </div>`)
     }
     $('.active_bar .stat_item .ui.icon.button').click(removeStat);
+    $('.active_bar .stat_item .ui.input input').change(updateNumbers);
+    renderEquation();
 }
 
-function updateAllStats(clearActive=true){
+function updateNumbers() {
+    let name = $(this)[0].name;
+    let index = 0;
+    if (name.startsWith('exp_')) {
+        index = 1;
+        name = name.substring(4);
+    } else {
+        index = 0;
+        name = name.substring(5);
+    }
+    let entry = dic_act_stats[name];
+    entry[index] = parseFloat($(this).val());
+    dic_act_stats[name] = entry;
+    renderEquation();
+}
+
+function updateAllStats(clearActive = true) {
     let cont = $('.stat_bar #stat_container');
     cont.empty();
-    for (let i = 0; i<arr_all_stats.length; i++){
+    for (let i = 0; i < arr_all_stats.length; i++) {
         stat = arr_all_stats[i]
         cont.append(`<div class="stat_item">
             <button class="ui icon button" name="btn_${stat}">
@@ -57,22 +87,83 @@ function updateAllStats(clearActive=true){
         </div>`)
     }
     $('.stat_bar .stat_item .ui.icon.button').click(addStat);
-    if (clearActive){
-        arr_act_stats = [];
+    if (clearActive) {
+        dic_act_stats = {};
         updateActiveStats();
     }
 }
 
-function getGroupInfo(name, text, choice){
+function buildMarkupString(dict) {
+    let ret = "";
+    if (Object.keys(dict).length == 0) {
+        ret = "1";
+    } else {
+        for (stat in dict) {
+            let entry = dict[stat];
+            ret += entry[0];
+            ret += " \\times ";
+            ret += stat.replace(/\d+$/, "");
+            ret += "^{";
+            ret += entry[1];
+            ret += "} + ";
+        }
+    }
+    return ret;
+}
+
+function renderEquation() {
+    let numerator = {};
+    let denominator = {};
+    //First pass to separate
+    for (stat in dic_act_stats) {
+        let entry = dic_act_stats[stat];
+        if (entry[1] > 0) {
+            numerator[stat] = entry.slice();
+        } else {
+            denominator[stat] = entry.slice();
+            denominator[stat][1] *= -1; //Make positive now that we know its on the bottom
+        }
+    }
+    let numer_string = buildMarkupString(numerator);
+    if (numer_string.endsWith(' + ')) {
+        numer_string = numer_string.slice(0, -3);
+    }
+    let denom_string = buildMarkupString(denominator);
+    if (denom_string.endsWith(' + ')) {
+        denom_string = denom_string.slice(0, -3);
+    }
+    $('.equation_box').empty();
+    $('.equation_box').append(`$$\{${numer_string}\\over${denom_string}\}$$`);
+    MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
+}
+
+function getGroupInfo(name, text, choice) {
     $.ajax({
         type: 'POST',
         url: '/groupinfo',
         data: {
             'group': name
         },
-        success: function(data){
+        success: function(data) {
             arr_all_stats = data;
             updateAllStats();
+        },
+        error: console.log
+    });
+}
+
+function submitStat() {
+    $.ajax({
+        type: 'POST',
+        url: '/visualize',
+        data: {
+            'group': $('.stat_bar .ui.fluid.selection.dropdown input')[0].value,
+            'equation': JSON.stringify(dic_act_stats),
+            'name': $('#stat_name')[0].value
+        },
+        success: function(data) {
+            $('.visualization').remove();
+            $('.center_content').append(data);
         },
         error: console.log
     });
